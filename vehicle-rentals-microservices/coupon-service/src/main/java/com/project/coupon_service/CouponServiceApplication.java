8500a5a49d5c38a60226fb3f27a8ad399df683ed
dsvasudev19 <vasudevds1729@gmail.com>
2024-10-05 17:16:11 +0530

    Added Mail Service , Review Service , Coupon Service

---
+++ vehicle-rentals-microservices/coupon-service/src/main/java/com/project/coupon_service/CouponServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.coupon_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponServiceApplication.class, args);
+	}
+
+}
---
+++ vehicle-rentals-microservices/coupon-service/src/test/java/com/project/coupon_service/CouponServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.coupon_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
