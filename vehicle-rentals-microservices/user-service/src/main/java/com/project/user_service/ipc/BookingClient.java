12bd2dd3b1705d5b773522e5cf59e6915917a629
dsvasudev19 <vasudevds1729@gmail.com>
2024-10-12 21:19:13 +0530

    Updated Feign Clients

--- vehicle-rentals-microservices/user-service/src/main/java/com/project/user_service/ipc/BookingClient.java
+++ vehicle-rentals-microservices/user-service/src/main/java/com/project/user_service/ipc/BookingClient.java
@@ -1,14 +1,20 @@
 package com.project.user_service.ipc;
 
-import org.springframework.web.service.annotation.GetExchange;
-import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.project.user_service.model.BookingPojo;
 
-import reactor.core.publisher.Flux;
-
+@FeignClient(value = "bookings-service",url="http://localhost:9000/bookings")
 public interface BookingClient {
 	
+	@GetMapping("/users/{id}")
+	public ResponseEntity<BookingPojo>getBookingByUserId(@PathVariable("id") long userId);
 
-	
+	@PostMapping
+	public ResponseEntity<BookingPojo> addBooking(@RequestBody BookingPojo bookingPojo);
 }
--- vehicle-rentals-microservices/vehicles-service/src/main/java/com/project/vehicles_service/ipc/BookingClient.java
+++ vehicle-rentals-microservices/vehicles-service/src/main/java/com/project/vehicles_service/ipc/BookingClient.java
@@ -1,14 +1,18 @@
 package com.project.vehicles_service.ipc;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.service.annotation.GetExchange;
-import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.project.vehicles_service.models.BookingPojo;
 
-import reactor.core.publisher.Flux;
-
-
+@FeignClient(value="bookings-service",url="http://localhost:9000/bookings")
 public interface BookingClient {
+	
+	@GetMapping("/vehicle/{id}")
+	public ResponseEntity<BookingPojo>getBookingByVehicleId(@PathVariable ("id")long vehicleId);
 	
 }
